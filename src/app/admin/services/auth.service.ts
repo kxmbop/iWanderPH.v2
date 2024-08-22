2cd50c81295a09e268a684d445f96a9d040253da
Kxmbrly Mae Alipin <93648323+kxmbrlydesu@users.noreply.github.com>
2024-08-22 10:48:02 +0800

    asdfasdf

--- src/app/admin/services/auth.service.ts
+++ src/app/admin/services/auth.service.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../../environments/environment'; // Import the environment
-
+import { environment } from '../../../environments/environment'; 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private baseUrl = environment.apiUrl;  // Use the environment variable
+  private baseUrl = environment.apiUrl;  
 
   constructor(private http: HttpClient) {}
 
@@ -19,3 +18,4 @@ export class AuthService {
   }
 
 }
+
