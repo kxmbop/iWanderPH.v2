16cd7197fb604141c2c46fcc0adcdf5b15754d98
Kxmbrly Mae Alipin <93648323+kxmbrlydesu@users.noreply.github.com>
2024-09-23 14:25:53 +0800

    adsa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'admin', pathMatch: 'full' },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: LandingPageComponent },
     { path: 'admin', loadChildren: ()=>import('./admin/admin.module').then(m=>m.AdminModule) },
     { path: 'merchant', loadChildren: ()=>import('./merchant/merchant.module').then(m=>m.MerchantModule) }
 ];
