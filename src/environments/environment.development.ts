e61893f044afc42dd748f855772a71743097e26d
Kxmbrly Mae Alipin <93648323+kxmbrlydesu@users.noreply.github.com>
2024-09-24 13:27:22 +0800

    switching to localhost

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: false,
     //apiUrl: 'https://easy-shrimp-dominant.ngrok-free.app/iwanderph_backend/api'
     //apiUrl: 'https://6797-216-247-18-142.ngrok-free.app'
-    apiUrl: 'https://980t3cc1-80.asse.devtunnels.ms/iwanderph_backend/api'
-    //apiUrl: 'http://localhost/iwanderph_backend/api'
+    //apiUrl: 'https://980t3cc1-80.asse.devtunnels.ms/iwanderph_backend/api'
+    apiUrl: 'http://localhost/iwanderph_backend/api'
   };
