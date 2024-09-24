ee224515921d6d15cc7a5c3ead28f80eca0a877b
Kxmbrly Mae Alipin <93648323+kxmbrlydesu@users.noreply.github.com>
2024-09-24 11:34:11 +0800

    asda

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost/iwanderph_backend/api'
+    //apiUrl: 'http://localhost/iwanderph_backend/api'
+  apiUrl: 'https://980t3cc1-80.asse.devtunnels.ms/iwanderph_backend/api'
   };
