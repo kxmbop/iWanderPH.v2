e0236cfb282fc663538b5ee363d63f158f1a850c
Melvin <melvinpadayao8@gmail.com>
2024-11-14 21:15:23 +0800

    listing new frontend

---
+++ src/app/merchant/services/listing.service.ts
@@ -0,0 +1,82 @@
+import { Injectable } from '@angular/core'; 
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ListingService {
+
+  private apiUrl = `${environment.apiUrl}/merchant/listing.php`;
+
+  constructor(private http: HttpClient) { }
+
+  getRooms(token: string): Observable<any> {
+    const params = new HttpParams().set('token', token);
+    return this.http.get(this.apiUrl, { params });
+  }
+
+  // Add room using POST method
+  addRoom(data: any, token: string): Observable<any> {
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.post<any>(this.apiUrl + `?token=${token}`, data, { headers });
+  }
+
+  deleteRoom(roomID: number, token: string): Observable<any> {
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    const options = {
+      headers: headers,
+      body: { RoomID: roomID } // Include RoomID in the body
+    };
+    return this.http.request<any>('DELETE', `${this.apiUrl}?token=${token}`, options);
+  }
+
+  // Fetch specific room data by ID
+  getRoomById(roomId: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${roomId}`);
+  }
+
+  // Update room data
+  updateRoom(roomId: number, updatedData: any): Observable<any> {
+    updatedData.RoomID = roomId;
+    return this.http.post<any>(`${environment.apiUrl}/merchant/update_listing.php`, updatedData);
+}
+
+getVehicles(token: string): Observable<any> {
+  return this.http.post(`${environment.apiUrl}/merchant/vehicles.php`, { token });
+}
+
+
+addVehicle(data: any, token: string): Observable<any> {
+  const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+  return this.http.post<any>(`${this.apiUrl}/add_vehicle`, data, { headers });
+}
+
+updateVehicle(vehicleID: number, updatedData: any): Observable<any> {
+  updatedData.VehicleID = vehicleID;
+  return this.http.post<any>(`${this.apiUrl}/update_vehicle`, updatedData);
+}
+
+deleteVehicle(vehicleID: number, token: string): Observable<any> {
+  const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+  const options = {
+    headers: headers,
+    body: { VehicleID: vehicleID }
+  };
+  return this.http.request<any>('DELETE', `${this.apiUrl}/delete_vehicle`, options);
+}
+
+getRoomView(roomId: number): Observable<any> {
+  return this.http.get<any>(`${this.apiUrl}/getRoomView.php?RoomID=${roomId}`);
+}
+
+getRoomInclusions(roomId: number): Observable<any> {
+  return this.http.get<any>(`${this.apiUrl}/getRoomInclusions.php?RoomID=${roomId}`);
+}
+
+getRoomGallery(roomId: number): Observable<any> {
+  return this.http.get<any>(`${this.apiUrl}/getRoomGallery.php?RoomID=${roomId}`);
+}
+
+}
